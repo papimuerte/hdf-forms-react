6b8f72881166a33e252090a3523c28a8a5ce304e
csulit <chrisgelosulit@gmail.com>
2021-07-30 22:42:39 +0800

    Make button mobile responsive

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,21 +3,21 @@ import { Button } from 'antd';
 const Dashboard: React.FC = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <div className='grid gap-28 md:grid-cols-3'>
+      <div className='grid w-full gap-28 md:grid-cols-3'>
         <Button
-          className='w-40 font-bold transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+          className='w-full font-bold transition duration-500 ease-in-out transform md:w-40 hover:-translate-y-1 hover:scale-110'
           type='primary'
         >
           Member
         </Button>
         <Button
-          className='w-40 font-bold transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+          className='w-full font-bold transition duration-500 ease-in-out transform md:w-40 hover:-translate-y-1 hover:scale-110'
           type='primary'
         >
           Event
         </Button>
         <Button
-          className='w-40 font-bold transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+          className='w-full font-bold transition duration-500 ease-in-out transform md:w-40 hover:-translate-y-1 hover:scale-110'
           type='primary'
         >
           Guest
